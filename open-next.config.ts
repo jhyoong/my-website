31d5edcf766d65f37c46c75498cfcdb22e3f7e8a
yoongjh <yoongjh95@gmail.com>
2025-06-26 23:19:49 +0800

    fix cloudflare configs

--- open-next.config.ts
+++ open-next.config.ts
@@ -4,9 +4,22 @@ module.exports = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
-  buildCommand: "npm run build",
-  // Disable incremental cache
-  disableIncrementalCache: true,
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 };
